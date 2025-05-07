a0d2a124fe87fdddf7813da2a260f4b8d14bb175
Rohit Upadhya <rohitupadhya18@gmail.com>
2025-05-07 18:55:13 +0000

    feat : added post procssor

---
+++ src/postprocessing/parser.py
@@ -0,0 +1,14 @@
+import json
+
+
+class Parser:
+    def __init__(
+        self,
+    ):
+        self.error_list = []
+        self.parsable = True
+
+    def parse(
+        self,
+    ):
+        pass
